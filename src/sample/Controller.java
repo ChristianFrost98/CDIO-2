587ff4466fcb961036a3a5a8dfafe0d1c0bdb962
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2019-10-21 16:43:52 +0200

    Lidt mere sjov test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,23 @@
 package sample;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class Controller {
 
     @FXML
     private Label labelTest;
 
+    @FXML
+    private ColorPicker colorPicker;
+
     public void btnGudPressed(ActionEvent actionEvent) {
+        labelTest.setTextFill(colorPicker.getValue());
         labelTest.setText("GUD KLIKKET");
-    }
-    public void btnBetterPressed(ActionEvent actionEvent) {
-        labelTest.setText("NOGET ANDET KLIKKET");
     }
 }
 
