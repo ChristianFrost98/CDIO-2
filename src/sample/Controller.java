b264b4a1c5efdad75d229ddd2d47a961fcd089af
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2019-10-21 16:46:21 +0200

    Lidt mere sjov test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,10 +3,12 @@ package sample;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 
+//Alt dette er bare lige for at lære at bruge JavaFX og Scene Builder..
 public class Controller {
 
     @FXML
@@ -14,6 +16,10 @@ public class Controller {
 
     @FXML
     private ColorPicker colorPicker;
+
+    @FXML
+    private Button btnGud;
+    //btnGud bliver lige nu ikke brugt. Set btnGudPressed. I SceneBuilder der kan man enten sætte ID på noget eller kalde en funktion når man gør noget. (Se under code i Scene Builder)
 
     public void btnGudPressed(ActionEvent actionEvent) {
         labelTest.setTextFill(colorPicker.getValue());
