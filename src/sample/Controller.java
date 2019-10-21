4593ae09786984793f21952de1d9c9e7f2be794f
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2019-10-21 16:10:48 +0200

    Test af javafx

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,9 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void btnGudPressed(ActionEvent actionEvent) {
+        System.out.println("GUD KNAPPEN KLIKKET!");
+    }
 }
