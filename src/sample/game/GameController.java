0f3e36ce5d4863583b79bc6c95b9b97b84a5bb2c
Christian Frost <s184140@student.dtu.dk>
2019-10-27 17:17:43 +0100

    Felter farveskift + resultat logik færdigt + Første udgave af spillet er udarbejdet

--- src/sample/game/GameController.java
+++ src/sample/game/GameController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.converter.IntegerStringConverter;
@@ -13,9 +14,32 @@ import sample.player.PlayerModel;
 
 import java.io.IOException;
 
+import static javafx.scene.paint.Color.BLUE;
+import static javafx.scene.paint.Color.GREEN;
+
 public class GameController {
     @FXML
     private Button throwDiceBtn;
+    @FXML
+    private Label feltTo;
+    @FXML
+    private Label feltTre;
+    @FXML
+    private Label feltFire;
+    @FXML
+    private Label feltFem;
+    @FXML
+    private Label feltSeks;
+    @FXML
+    private Label feltSyv;
+    @FXML
+    private Label feltOtte;
+    @FXML
+    private Label feltNi;
+    @FXML
+    private Label feltTi;
+    @FXML
+    private Label feltElve;
     @FXML
     private Label feltTolv;
     @FXML
@@ -27,6 +51,8 @@ public class GameController {
     @FXML
     private Label playerTwoMoney;
     @FXML
+    private Label resultatTekstFelt;
+    @FXML
     private Label currentPlayerLabel;
 
     private PlayerModel p1,p2;
@@ -34,6 +60,7 @@ public class GameController {
 
     public void throwDiceBtn(ActionEvent event) throws IOException {
         setCurrentPlayerName();
+        resetColor();
 
         DieModel dice1 = new DieModel();
         dice1.rollDice();
@@ -50,42 +77,64 @@ public class GameController {
         switch (dice1Number + dice2Number) {
             case 2:
                 currentPlayer.addMoney(250);
+                feltTo.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du reddede prinsessen i tårnet, og kongen giver dig 250 mønter!");
                 break;
             case 3:
                 currentPlayer.addMoney(-100);
+                feltTre.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du faldt ned i et krater, og tabte 100 mønter derned");
                 break;
             case 4:
                 currentPlayer.addMoney(100);
+                feltFire.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Portene til Paladset åbner for dig, og som velkomst får du 100 mønter");
                 break;
             case 5:
                 currentPlayer.addMoney(-20);
+                feltFem.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du er faret vildt i den kolde ørken, og bruger 20 mønter på at holde varmen");
                 break;
             case 6:
                 currentPlayer.addMoney(180);
+                feltSeks.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du finder sikkerhed i en by omringet af mure, +180");
                 break;
             case 7:
                 currentPlayer.addMoney(0);
+                feltSyv.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du overnatter i et kloster, +0");
                 break;
             case 8:
                 currentPlayer.addMoney(-70);
+                feltOtte.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du falder ned i en sort grotte, -70");
                 break;
             case 9:
                 currentPlayer.addMoney(60);
+                feltNi.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du finder ly i en hytte i bjergene, +60");
                 break;
             case 10:
                 currentPlayer.addMoney(-80);
+                feltTi.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du bliver overfaldet af ulve-muren, de stjæler 80 mønter men du får en ekstra tur");
                 break;
             case 11:
                 currentPlayer.addMoney(-50);
+                feltElve.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du bliver smidt i “hullet” og mister samtidigt 50 mønter");
                 break;
             case 12:
                 currentPlayer.addMoney(650);
+                feltTolv.setStyle("-fx-background-color:GREEN;");
+                resultatTekstFelt.setText("Du fandt en guldmine og får 650 mønter!");
                 break;
         }
         playerOneMoney.setText(Integer.toString(p1.getMoney()));
         playerTwoMoney.setText(Integer.toString(p2.getMoney()));
         if (p1.getMoney()>=3000 || p2.getMoney()>=3000){
-            System.exit(0);
+            resultatTekstFelt.setText("GAMEOVER...Vinderen er: " + currentPlayer);
         }
     }
 
@@ -113,4 +162,18 @@ public class GameController {
         }
         GameController.this.setCurrentPlayerName();
     }
+    public void resetColor(){
+        feltTo.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltTre.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltFire.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltFem.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltSeks.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltSyv.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltOtte.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltNi.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltTi.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltElve.setStyle("-fx-background-color:rgba(0,0,0,0);");
+        feltTolv.setStyle("-fx-background-color:rgba(0,0,0,0);");
+    }
+
 }
--- src/sample/game/GameModel.java
+++ src/sample/game/GameModel.java
@@ -1,8 +1,13 @@
 package sample.game;
 
+import javafx.event.ActionEvent;
 import sample.player.PlayerModel;
+import java.io.IOException;
+import sample.die.DieModel;
 
 public class GameModel {
+
+
 
 
 }
